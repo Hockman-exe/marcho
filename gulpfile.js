a18b52b62fd8327d16c6a044ceaa667913d712bf
Hockman-exe <Hokkeist604@gmail.com>
2024-10-17 04:53:28 +0300

    addet header and fonts

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,8 @@ function browsersync() {
 
 function styles () {
   return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss())
+    // .pipe(scss({outputStyle: 'compressed'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
